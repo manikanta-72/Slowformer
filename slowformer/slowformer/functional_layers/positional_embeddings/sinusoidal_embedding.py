d5916c075e8cbdd2d19e9ace85388625987d5a04
Manikanta Loya <mani.4272@gmail.com>
2024-11-16 19:50:52 -0800

    variable renaming to avoid confusion

--- slowformer/slowformer/functional_layers/positional_embeddings/sinusoidal_embedding.py
+++ slowformer/slowformer/functional_layers/positional_embeddings/sinusoidal_embedding.py
@@ -12,11 +12,11 @@ FREQUENCY = 10_000
 class SinusoidalEmbedding(nn.Module):
     """Enriches Positional information (constant sinusoidal vectors)"""
 
-    def __init__(self, hidden_dim: int, max_positions: int) -> None:
+    def __init__(self, d_model: int, max_positions: int) -> None:
         super().__init__()
-        self.hidden_dim = hidden_dim
+        self.d_model = d_model
         self.max_positions = max_positions
-        pos_info = compute_sinusoidal_vectors(max_positions, hidden_dim)
+        pos_info = compute_sinusoidal_vectors(max_positions, d_model)
         self.register_buffer("pos_info", pos_info)
 
     def forward(self, inputs: T.Tensor) -> T.Tensor:
@@ -24,12 +24,12 @@ class SinusoidalEmbedding(nn.Module):
         return inputs + self.pos_info[:, : inputs.size(1)]
 
 
-def compute_sinusoidal_vectors(max_positions: int, hidden_dim: int) -> T.Tensor:
+def compute_sinusoidal_vectors(max_positions: int, vector_len: int) -> T.Tensor:
     """Computes constant vector using sinusoidal wave functions"""
-    pos_vectors = T.zeros(max_positions, hidden_dim)
+    pos_vectors = T.zeros(max_positions, vector_len)
     positions = T.arange(0, max_positions, dtype=T.float).unsqueeze(1)
     mul_coeff = T.exp(
-        T.arange(0, hidden_dim, 2) * (-1 * math.log(FREQUENCY) / hidden_dim)
+        T.arange(0, vector_len, 2) * (-1 * math.log(FREQUENCY) / vector_len)
     )
     pos_vectors[:, 0::2] = T.sin(positions * mul_coeff)
     pos_vectors[:, 1::2] = T.cos(positions * mul_coeff)
